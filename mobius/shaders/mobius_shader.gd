83043a29e873eea8b60d6ddabf9cb82171e6c04f
Alex Kam <kamzhanyue@gmail.com>
2026-01-17 21:01:08 +0900

    progress on mobius shader

--- mobius/shaders/mobius_shader.gd
+++ mobius/shaders/mobius_shader.gd
@@ -5,15 +5,29 @@ extends CompositorEffect
 var rd: RenderingDevice
 var shader: RID
 var pipeline: RID
+var nearest_sampler: RID
+var parameter_storage_buffer: RID
 
 func _init() -> void:
 	effect_callback_type = EFFECT_CALLBACK_TYPE_POST_TRANSPARENT
 	rd = RenderingServer.get_rendering_device()
 	RenderingServer.call_on_render_thread(_initialise_compute)
+	var data := PackedFloat32Array()
+	data.resize(20)
+	data.fill(0)
+	var parameter_data := data.to_byte_array()
+	parameter_storage_buffer = rd.storage_buffer_create(parameter_data.size(), parameter_data)
+
 
 func _notification(what: int) -> void:
-	if what == NOTIFICATION_PREDELETE and shader.is_valid():
-		rd.free_rid(shader)
+	if what == NOTIFICATION_PREDELETE:
+		if shader.is_valid():
+			RenderingServer.free_rid(shader)
+		if nearest_sampler.is_valid():
+			rd.free_rid(nearest_sampler)
+		if parameter_storage_buffer.is_valid():
+			rd.free_rid(parameter_storage_buffer)
+
 
 func _initialise_compute() -> void:
 	rd = RenderingServer.get_rendering_device()
@@ -26,41 +40,69 @@ func _initialise_compute() -> void:
 	shader = rd.shader_create_from_spirv(shader_spirv)
 	if shader.is_valid():
 		pipeline = rd.compute_pipeline_create(shader)
-
 func _render_callback(p_effect_callback_type: EffectCallbackType, p_render_data: RenderData) -> void:
 	if not(rd and p_effect_callback_type == EFFECT_CALLBACK_TYPE_POST_TRANSPARENT and pipeline.is_valid()):
 		return
 
 	var render_scene_buffers := p_render_data.get_render_scene_buffers()
-	if not render_scene_buffers:
-		return
+	var scene_data := p_render_data.get_render_scene_data()
+	if render_scene_buffers and scene_data:
+		var size: Vector2i = render_scene_buffers.get_internal_size()
+		if size.x == 0 and size.y == 0:
+			return
+		# We can use a compute shader here.
+		@warning_ignore("integer_division")
+		var x_groups := (size.x - 1) / 8 + 1
+		@warning_ignore("integer_division")
+		var y_groups := (size.y - 1) / 8 + 1
+		var z_groups := 1
 
-	var size: Vector2i = render_scene_buffers.get_internal_size()
-	if size.x == 0 and size.y == 0:
-		return
-	# We can use a compute shader here.
-	@warning_ignore("integer_division")
-	var x_groups := (size.x - 1) / 8 + 1
-	@warning_ignore("integer_division")
-	var y_groups := (size.y - 1) / 8 + 1
-	var z_groups := 1
-	var push_constant := PackedFloat32Array([ size.x, size.y, 0.0, 0.0, ])
+		# Make sure we have a sampler
+		if not nearest_sampler.is_valid():
+			var sampler_state := RDSamplerState.new()
+			sampler_state.min_filter = RenderingDevice.SAMPLER_FILTER_NEAREST
+			sampler_state.mag_filter = RenderingDevice.SAMPLER_FILTER_NEAREST
+			nearest_sampler = rd.sampler_create(sampler_state)
 
-	var view_count: int = render_scene_buffers.get_view_count()
+		var view_count: int = render_scene_buffers.get_view_count()
+		for view in view_count:
+			var colour_buffer: RID = render_scene_buffers.get_color_layer(view)
+			var depth_buffer: RID = render_scene_buffers.get_depth_layer(view)
 
-	for view in view_count:
-		var input_image: RID = render_scene_buffers.get_color_layer(view)
-		var uniform := RDUniform.new()
-		uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
-		uniform.binding = 0
-		uniform.add_id(input_image)
-		var uniform_set := UniformSetCacheRD.get_cache(shader, 0, [uniform])
+			var parameters := PackedFloat32Array([size.x, size.y, 0.0, 0.0])
+			var inv_proj_mat := scene_data.get_cam_projection().inverse()
+			var inv_proj_mat_array := PackedVector4Array([inv_proj_mat.x, inv_proj_mat.y, inv_proj_mat.z, inv_proj_mat.w])
 
-		# Run the compute shader
-		var compute_list := rd.compute_list_begin()
-		rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
-		rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
-		rd.compute_list_set_push_constant(compute_list, push_constant.to_byte_array(), push_constant.size() * 4)
-		rd.compute_list_dispatch(compute_list, x_groups, y_groups, z_groups)
-		rd.compute_list_end()
+			var parameter_data = parameters.to_byte_array()
+			parameter_data.append_array(inv_proj_mat_array.to_byte_array())
+			rd.buffer_update(parameter_storage_buffer, 0, parameter_data.size(), parameter_data)
+
+			# Uniform Buffer
+			var storage_buffer := RDUniform.new()
+			storage_buffer.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+			storage_buffer.binding = 0
+			storage_buffer.add_id(parameter_storage_buffer)
+
+			# Colour Image
+			var uniform_colour := RDUniform.new()
+			uniform_colour.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
+			uniform_colour.binding = 1
+			uniform_colour.add_id(colour_buffer)
+
+			# Depth Buffer
+			var uniform_depth := RDUniform.new()
+			uniform_depth.uniform_type = RenderingDevice.UNIFORM_TYPE_SAMPLER_WITH_TEXTURE
+			uniform_depth.binding = 2
+			uniform_depth.add_id(nearest_sampler)
+			uniform_depth.add_id(depth_buffer)
+
+			var uniform_set := UniformSetCacheRD.get_cache(shader, 0, [storage_buffer, uniform_colour, uniform_depth])
+
+			# Run the compute shader
+			var compute_list := rd.compute_list_begin()
+			rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
+			rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
+			# rd.compute_list_set_push_constant(compute_list, push_constant.to_byte_array(), push_constant.size() * 4)
+			rd.compute_list_dispatch(compute_list, x_groups, y_groups, z_groups)
+			rd.compute_list_end()
 
--- mobius/shaders/mobius_shader.glsl
+++ mobius/shaders/mobius_shader.glsl
@@ -3,28 +3,65 @@
 
 layout (local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
-layout (rgba16f, set = 0, binding = 0) uniform image2D colour_image;
-
-layout (push_constant, std430) uniform Params {
+layout (set = 0, binding = 0, std430) readonly buffer Params {
   vec2 raster_size;
   vec2 reserved;
+  mat4 inv_projection_mat;
 } params;
+
+layout (rgba16f, set = 0, binding = 1) uniform image2D colour_buffer;
+layout (set = 0, binding = 2) uniform sampler2D depth_buffer;
+
+vec2 offsets[9] = vec2[](
+  vec2(-1, 1),    // top left
+  vec2(0, 1),       // top center
+  vec2(1, 1),     // top right
+  vec2(-1, 0),      // center left
+  vec2(0, 0),         // center center
+  vec2(1, 0),       // center right
+  vec2(-1, -1),   // bottom left
+  vec2(0, -1),      // bottom center
+  vec2(1, -1)    // bottom right
+);
+
+int sobel_vertical_kernel[9] = int[](
+    1, 0, -1,
+    2, 0, -2,
+    1, 0, -1
+);
+
+int sobel_horizontal_kernel[9] = int[](
+    1, 2, 1,
+    0, 0, 0,
+    -1, -2, -1
+);
+
+// Unproject
+
 
 void main() {
   ivec2 uv = ivec2(gl_GlobalInvocationID.xy);
-  ivec2 size = ivec2(params.raster_size);
+  vec2 size = vec2(params.raster_size);
+  vec2 uv_normalised = uv / size;
+  vec2 texel_size = 1.0 / size.xy;
 
   // Prevent reading / writing out of bounds
   if (uv.x >= size.x || uv.y >= size.y) {
     return;
   }
 
-  // Read from the colour buffer
-  vec4 colour = imageLoad(colour_image, uv);
+  vec4 sampleTex[9];
+  for (int i = 0; i < 9; ++i) {
+    sampleTex[i] = texture(depth_buffer, uv_normalised + offsets[i] * texel_size);
+  }
 
-  float gray = (colour.r + colour.g + colour.g) / 3;
-  colour.rgb = vec3(gray);
+  vec4 colour = vec4(0);
+  for (int i = 0; i < 9; ++i) {
+    colour += sampleTex[i] * sobel_vertical_kernel[i];
+    colour += sampleTex[i] * sobel_horizontal_kernel[i];
+  }
+  // vec4 colour = texture(depth_buffer, uv_normalised);
 
   // Write back to the colour buffer
-  imageStore(colour_image, uv, colour);
+  imageStore(colour_buffer, uv, colour);
 }
