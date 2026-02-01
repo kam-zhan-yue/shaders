1876daa31371b87e9e8ffa5d317ce14507ff8a65
Alex Kam <kamzhanyue@gmail.com>
2026-02-01 20:28:38 +0900

    feat: pipe in depth and normal into pixel shader

--- pixel/pixel_shader.glsl
+++ pixel/pixel_shader.glsl
@@ -9,6 +9,18 @@ layout (set = 0, binding = 0, std430) readonly buffer Params {
 } params;
 
 layout (rgba16f, set = 0, binding = 1) uniform image2D colour_buffer;
+layout (set = 0, binding = 2) uniform sampler2D depth_buffer;
+layout (set = 0, binding = 3) uniform sampler2D normal_buffer;
+
+// Taken from https://github.com/godotengine/godot-docs/issues/9591
+vec4 normal_roughness_compatibility(vec4 p_normal_roughness) {
+	float roughness = p_normal_roughness.w;
+	if (roughness > 0.5) {
+		roughness = 1.0 - roughness;
+	}
+	roughness /= (127.0 / 255.0);
+	return vec4(normalize(p_normal_roughness.xyz * 2.0 - 1.0) * 0.5 + 0.5, roughness);
+}
 
 void main() {
   ivec2 uv = ivec2(gl_GlobalInvocationID.xy);
@@ -21,4 +33,12 @@ void main() {
   float grayscale = (colour.x + colour.y + colour.z) / 3;
   vec4 gray = vec4(vec3(grayscale), 1.0);
   imageStore(colour_buffer, uv, gray);
+
+  float depth = texture(depth_buffer, uv_normalised).r;
+  imageStore(colour_buffer, uv, vec4(vec3(depth), 1.0));
+
+  vec4 normal = texture(normal_buffer, uv_normalised);
+  normal = normal_roughness_compatibility(normal);
+  normal = normalize(normal * 2.0 - 1.0);
+  imageStore(colour_buffer, uv, normal);
 }
